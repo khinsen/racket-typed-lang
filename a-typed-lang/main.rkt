da05d39e3f3f6349fe1e4508214e798f23d6e08e
AlexKnauth <alexander@knauth.org>
2014-10-20 18:12:35 -0400

    this seems to work so far

--- a-typed-lang/main.rkt
+++ a-typed-lang/main.rkt
@@ -1,18 +1,20 @@
 #lang typed-racket/minimal
 
 (require typed/racket
-         a-typed-lang/more
+         ;a-typed-lang/more ; for some reason this can't be here
          (only-in typed/racket
                   [#%module-begin tr:module-begin]
                   [require tr:require]))
 
 (provide (except-out (all-from-out typed/racket) #%module-begin)
          (rename-out [module-begin #%module-begin])
-         (all-from-out a-typed-lang/more))
+         ;(all-from-out a-typed-lang/more)
+         )
 
 (define-syntax module-begin
-  (syntax-rules (require)
-    [(_ decl ...)
-     (tr:module-begin
-      (require a-typed-lang/more)
-      decl ...)]))
+  (lambda (stx)
+    (syntax-case stx ()
+      [(_ decl ...)
+       #`(tr:module-begin
+          #,(datum->syntax stx '(require a-typed-lang/more))
+          decl ...)])))
