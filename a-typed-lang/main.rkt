4c4f871c07bda77bea029ebab7ef212409bd9112
Konrad Hinsen <konrad.hinsen@fastmail.net>
2014-10-27 09:48:07 +0100

    Move (require a-typed-lang/more) into the lexical context of the caller
    This fix was suggested by Evan Moran, see the discussion at:
    
     http://www.mail-archive.com/users@racket-lang.org/msg24601.html

--- a-typed-lang/main.rkt
+++ a-typed-lang/main.rkt
@@ -1,18 +1,21 @@
 #lang typed-racket/minimal
 
 (require typed/racket
-         a-typed-lang/more
          (only-in typed/racket
                   [#%module-begin tr:module-begin]
                   [require tr:require]))
 
 (provide (except-out (all-from-out typed/racket) #%module-begin)
-         (rename-out [module-begin #%module-begin])
-         (all-from-out a-typed-lang/more))
+         (rename-out [module-begin #%module-begin]))
 
-(define-syntax module-begin
-  (syntax-rules (require)
-    [(_ decl ...)
-     (tr:module-begin
-      (require a-typed-lang/more)
-      decl ...)]))
+(define-syntax (module-begin ix)
+  (syntax-case ix ()
+    [(_ rest ...)
+     (with-syntax
+       ([rix
+         ; tr:require keeps the meaning that it has in this module, but the
+         ; s-exp as a whole lands in the *caller's* lexical context...
+         (datum->syntax ix (list #'tr:require 'a-typed-lang/more))])
+       #'(tr:module-begin
+           rix
+           rest ...))]))
